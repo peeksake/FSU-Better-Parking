e9f78aa1ab0a0df9eb279c28ee56e72b02a3da31
JShelf <jpeek@vrsystems.com>
2019-04-11 21:42:02 -0400

    Converted Mainview to RecyclerView

--- app/src/main/java/com/parkingoracle/parkingoracle/MainActivity.java
+++ app/src/main/java/com/parkingoracle/parkingoracle/MainActivity.java
@@ -2,15 +2,14 @@ package com.parkingoracle.parkingoracle;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -24,22 +23,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private String className = MainActivity.class.getSimpleName();
-    List<Integer> maxCapacities = Arrays.asList(786, 839, 1186, 795, 1118, 928, 132, 288);
-    List<String> GarageCapacityList = new ArrayList<>();
-    TextView[] textViewArray;
+    List<String> maxCapacities = Arrays.asList("786","839", "1186", "795", "1118", "928", "132", "288");
+    List<String> garageNames = Arrays.asList("Call Street", "Saint Augustine Street","Spirit Way", "Traditions Way", "Pensacola Street", "Woodward Avenue", "Pensacola Street", "Woodward Avenue");
+    List<String> spotsOpen = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        textViewArray = new TextView[]{
-                findViewById(R.id.currentcall), findViewById(R.id.currentaug), findViewById(R.id.currentspirit),
-                findViewById(R.id.currenttraditions), findViewById(R.id.currentpen), findViewById(R.id.currentwood),
-                findViewById(R.id.currentcallf), findViewById(R.id.currentspiritf)};
+        initRecyclerView();
         new GetGarageCapacities().execute();
     }
+
     private class GetGarageCapacities extends AsyncTask<Void, Void, Void> {
         @Override
         protected void onPreExecute() {
@@ -59,10 +56,10 @@ public class MainActivity extends AppCompatActivity {
 
                     for (int i = 0; i < jsonArray.length(); i++) {
                         Integer capacity = (Integer) jsonArray.getJSONArray(i).get(1);
-                        if (capacity >= maxCapacities.get(i)) {
-                            GarageCapacityList.add(i, "FULL");
+                        if (capacity >= Integer.parseInt(maxCapacities.get(i))) {
+                            spotsOpen.add(i, "FULL");
                         } else {
-                            GarageCapacityList.add(i, Integer.toString(maxCapacities.get(i) - capacity));
+                            spotsOpen.add(i, Integer.toString(Integer.parseInt(maxCapacities.get(i)) - capacity));
                         }
                     }
                 } catch (final JSONException e) {
@@ -87,12 +84,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
-            for (int i = 0; i < textViewArray.length; i++) {
-                textViewArray[i].setText((GarageCapacityList.get(i)));
             }
         }
-    }
 
+        //Inflates the toolbar with options
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -100,12 +95,20 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //Makes buttons clickable
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
         return super.onOptionsItemSelected(item);
+    }
+
+    //Intializes the garageviews
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.RecyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, garageNames, maxCapacities, spotsOpen);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setElevation(1);
     }
 }
---
+++ app/src/main/java/com/parkingoracle/parkingoracle/RecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.parkingoracle.parkingoracle;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String Tag= "RecyclerView Adapter";
+
+    private List<String> mGarageNames = new ArrayList<>();
+    private List<String> mMaxCapacties = new ArrayList<>();
+    private List<String> mSpotsOpen = new ArrayList<>();
+    private Context mContext;
+
+    //Constructor
+    public RecyclerViewAdapter(Context context, List<String> GarageNames, List<String> MaxCapacties, List<String> SpotsOpen){
+        mContext = context;
+        mGarageNames = GarageNames;
+        mMaxCapacties = MaxCapacties;
+        mSpotsOpen = SpotsOpen;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        Log.d( Tag, "onCreateViewHolder: called.");
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.garageitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        Log.d( Tag, "onBindViewHolder: called.");
+
+        viewHolder.garageName.setText(mGarageNames.get(i));
+        viewHolder.maxCapacity.setText(mMaxCapacties.get(i));
+        viewHolder.spotsOpen.setText(mSpotsOpen.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSpotsOpen.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView garageName, maxCapacity, spotsOpen;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            garageName = itemView.findViewById(R.id.GarageName);
+            maxCapacity = itemView.findViewById(R.id.maxcapacity);
+            spotsOpen = itemView.findViewById(R.id.spotsopen);
+            parentLayout = itemView.findViewById(R.id.parent);
+        }
+    }
+}
